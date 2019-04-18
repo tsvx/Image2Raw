c0c101f83c102ce7ac996c9159ff16942b2d7880
Artem Sulimov <artem.sulimov@vocord.ru>
2019-04-18 19:04:17 +0300

    Add JpegDecoder console app (WIP)

--- JpegDecoder/Program.cs
+++ JpegDecoder/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,38 @@ namespace JpegDecoder
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: JpegDecoder <JPEG_file>");
+                return 0;
+            }
+            string jpegName = args[0];
+            try
+            {
+                var bmp = (Bitmap)Image.FromFile(jpegName);
+                var bytes = Decode(bmp, out var size);
+                SaveBitmap(jpegName, size, bytes);
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+
+        static byte[] Decode(Bitmap bmp, out Size size)
+        {
+            size = new Size(bmp.Width, bmp.Height);
+
+            return null;
+        }
+
+        static void SaveBitmap(string origName, Size size, byte[] data)
+        {
+
         }
     }
 }
